53792171d23dd92b6430950f05371b0f22efa5f6
Arthur <arthursousarabelo2@gmail.com>
2025-10-01 14:41:24 -0300

    feat(database): update foreign key constraints for user and organization relations with cascade and set default actions

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -35,14 +35,21 @@ export const resetPasswordRoute: FastifyPluginAsyncZod = async (app) => {
 
       const passwordHash = await hash(password, { hashLength: 6 })
 
-      await prisma.user.update({
-        where: {
-          id: tokenFromCode.userId,
-        },
-        data: {
-          passwordHash,
-        },
-      })
+      await prisma.$transaction([
+        prisma.user.update({
+          where: {
+            id: tokenFromCode.userId,
+          },
+          data: {
+            passwordHash,
+          },
+        }),
+        prisma.token.delete({
+          where: {
+            id: code,
+          },
+        }),
+      ])
 
       return reply.status(204).send()
     }
